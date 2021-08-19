b8c9da78d15fec33c31f4830bfba970fb181dac0
fryfritter <ryan.devqr@gmail.com>
2021-08-19 16:37:50 +0800

    WIP test case

--- src/App.test.js
+++ src/App.test.js
@@ -2,79 +2,80 @@ import {
   fireEvent,
   render,
   screen,
-  waitFor,
   waitForElement,
 } from "@testing-library/react";
 import App from "./App";
+import { waitFor } from "./utils/CustomWaitFor";
 
-// test("should find 2 links", () => {
-//   render(<App />);
-//   const linkElement_List = screen.getByText("List Cocktails");
-//   expect(linkElement_List).toBeInTheDocument();
+test("1. should find 2 links from main page", () => {
+  render(<App />);
+  const linkElement_List = screen.getByText("List Cocktails");
+  expect(linkElement_List).toBeInTheDocument();
 
-//   const linkElement_Recipe = screen.getByText("Cocktails Recipe");
-//   expect(linkElement_Recipe).toBeInTheDocument();
-// });
+  const linkElement_Recipe = screen.getByText("Cocktails Recipe");
+  expect(linkElement_Recipe).toBeInTheDocument();
+});
 
-// test("should display home when home clicked", () => {
-//   render(<App />);
-//   const linkElement_List = screen.getByText("List Cocktails");
+test("2. should display home when home clicked", () => {
+  render(<App />);
+  const linkElement_List = screen.getByText("List Cocktails");
 
-//   fireEvent.click(linkElement_List);
-//   expect(screen.getByText(/Pick/)).toBeInTheDocument();
-// });
-// test("should display MYOB when recipe is clicked", () => {
-//   render(<App />);
-//   const linkElement_Recipe = screen.getByText("Cocktails Recipe");
+  fireEvent.click(linkElement_List);
+  expect(screen.getByText(/Pick/)).toBeInTheDocument();
+});
 
-//   fireEvent.click(linkElement_Recipe);
-//   expect(screen.getByText(/MYOB/i)).toBeInTheDocument();
-// });
+test("3. should display MYOB when recipe is clicked", () => {
+  render(<App />);
+  const linkElement_Recipe = screen.getByText("Cocktails Recipe");
 
-// test("should find 3 buttons in list cocktails", () => {
-//   render(<App />);
-//   const linkElement_List = screen.getByText("List Cocktails");
+  fireEvent.click(linkElement_Recipe);
+  expect(screen.getByText(/MYOB/i)).toBeInTheDocument();
+});
 
-//   fireEvent.click(linkElement_List);
+test("4. should find 3 buttons in list cocktails", () => {
+  render(<App />);
+  const linkElement_List = screen.getByText("List Cocktails");
 
-//   const btn_A = screen.getByTestId("search-by-A");
-//   expect(btn_A).toBeInTheDocument();
+  fireEvent.click(linkElement_List);
 
-//   const btn_H = screen.getByTestId("search-by-H");
-//   expect(btn_H).toBeInTheDocument();
+  const btn_A = screen.getByTestId("search-by-A");
+  expect(btn_A).toBeInTheDocument();
 
-//   const btn_Z = screen.getByTestId("search-by-Z");
-//   expect(btn_Z).toBeInTheDocument();
-// });
+  const btn_H = screen.getByTestId("search-by-H");
+  expect(btn_H).toBeInTheDocument();
 
-test("should fetch responses when Z button is clicked", async () => {
+  const btn_Z = screen.getByTestId("search-by-Z");
+  expect(btn_Z).toBeInTheDocument();
+});
+
+test("5. should fetch responses when Z button is clicked", async () => {
   render(<App />);
   const linkElement_List = screen.getByText("List Cocktails");
 
   fireEvent.click(linkElement_List);
 
   const btn_Z = screen.getByTestId("search-by-Z");
-  fireEvent.click(btn_Z); //this would call API
-  // await waitFor(() => fireEvent.click(btn_Z));
-  // await waitFor(() => screen.getByTestId("cocktail-list-items"));cd ..
-  // await screen.getByTestId("cocktail-list-items");
-
-  // await waitFor(() =>
-  //   expect(screen.getAllByTestId("cocktail-list-items").length).toBeGreaterThan(
-  //     0
-  //   )
-  // );
-  //   const cocktails = await waitFor(() =>
-  //   screen.getAllByTestId("cocktail-list-items")
-  // );
-  let cocktails = await screen.findAllByTestId("cocktail-list-items");
-  cocktails = await waitFor(() =>
+  fireEvent.click(btn_Z);
+  let cocktails = await waitFor(() =>
     screen.findAllByTestId("cocktail-list-items")
   );
 
   expect(cocktails.length).toBeGreaterThan(0);
+});
 
-  // const responseCocktailItem = screen.getByTestId("cocktail-list-items");
+test("6. should go to detail page when first cocktail is clicked", async () => {
+  render(<App />);
+  const linkElement_List = screen.getByText("List Cocktails");
 
-  // expect(responseCocktailItem).toBeInTheDocument();
+  fireEvent.click(linkElement_List);
+
+  const btn_Z = screen.getByTestId("search-by-Z");
+  fireEvent.click(btn_Z);
+  let cocktails = await waitFor(() =>
+    screen.findAllByTestId("cocktail-list-items")
+  );
+  let cocktail = cocktails[0];
+  console.log(cocktail);
+  fireEvent.click(cocktail);
+  expect(screen.getByText(/MYOB/i)).toBeInTheDocument();
 });
